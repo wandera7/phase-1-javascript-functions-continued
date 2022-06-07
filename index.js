7a1d355e56a30bfe0a08c3ea3357f5efec14ef1a
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-07 08:21:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function mondayWork(task){ 
+  return task?`This Monday, I will ${task}.`: `This Monday, I will go to the office.`
+}
+function saturdayFun(activity){
+    return activity?`This Saturday, I want to ${activity}!`: `This Saturday, I want to roller-skate!`
+
+}
+function wrapAdjective(name="*"){
+    return function(day='special'){
+        return `You are ${name}${day}${name}!`
+
+    }
+}
